1c77d12e1e16625fa17e72044196fb603594d04b
Dima Ganichev <dimkadaya@yandex.ru>
2022-05-24 19:54:06 +0500

    add to commit

--- start.py
+++ start.py
@@ -1,1 +1,13 @@
-print('hello world')
+def max():
+  a = int(input('Введите первое число: '))
+  b = int(input('Введите второе число: '))
+  max = (a + b + abs(a - b)) // 2
+  return max
+
+max = max()
+c = int(input('Введите третье число: '))
+
+if c >= max:
+  print('Наибольшее число:', c)
+else:
+  print('Наибольшее число:', max)
